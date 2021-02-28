9528e2a0ebbb3a7327485e939c68850415834dd6
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 20:46:33 +0400

    Pushing to heroku

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 const server = "https://my-todo-app-2344wqs.herokuapp.com";
 // const server = "http://localhost:3000";
 
-async function getList() {
-    const list = await fetch(`${server}/get_items`)
+async function fetchList() {
+    // get all todos from server
+    return fetch(`${server}/get_items`)
         .then((res) => res.json())
         .then((res) => res.collection)
         .catch((err) => console.log(err));
+}
+
+async function putList() {
+    const list = await fetchList();
+
+    // put every element of fetched list into todo-list <ul>
     list.forEach((element) => {
         let node = document.createElement("li");
         node.innerText = element.data;
@@ -13,33 +20,46 @@ async function getList() {
     });
 }
 
-window.addEventListener("load", async (event) => {
-    // get entire list from database
-    await getList();
+async function insertTodo(todo) {
+    fetch(`${server}/add_item`, {
+        method: "POST", // or 'PUT'
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
+    })
+        .then((response) => response.text())
+        .then((text) => {
+            console.log(`${text}`);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
 
+function addSubmitListener() {
     document.getElementById("form").addEventListener("submit", (event) => {
+        // prevents from redirecting
         event.preventDefault();
+
+        // create and insert node == <li> text-input-value </li> into <ul>
         let node = document.createElement("li");
         node.innerText = document.getElementById("text-input").value;
         document.getElementById("todo-list").appendChild(node);
 
+        // clear text-input after submit
         document.getElementById("text-input").value = "";
-        // send list item to server so it can put it into mongodb
-        const data = { data: node.innerText };
 
-        fetch(`${server}/add_item`, {
-            method: "POST", // or 'PUT'
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => response.text())
-            .then((text) => {
-                console.log(`${text}`);
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
+        // send list item to server so it can insert it into mongodb
+        const todo = { data: node.innerText };
+        insertTodo(todo);
     });
+}
+
+window.addEventListener("load", async (event) => {
+    // get entire list from database
+    await putList();
+
+    // add listener for the submit button
+    addSubmitListener();
 });
