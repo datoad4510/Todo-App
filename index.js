dfd6ad363ed96b8381a8333a9f38d3b0f978ed10
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 16:46:27 +0400

    Pushing to heroku

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT;
-app.use(function (req, res, next) {
-    res.header(
-        "Access-Control-Allow-Origin",
-        "https://datoad4510.github.io/Todo-App/"
-    ); // update to match the domain you will make the request from
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
+
+app.use(express.static(path.join(__dirname, "Scripts")));
+
+// app.use(function (req, res, next) {
+//     res.header(
+//         "Access-Control-Allow-Origin",
+//         "https://datoad4510.github.io/Todo-App/"
+//     ); // update to match the domain you will make the request from
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// });
 
 app.use(cors());
 app.use(bodyParser.json());
