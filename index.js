e47c47d1bca0d144fb2e94b00cce7f111c5b41d1
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 16:50:01 +0400

    Pushing to heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "Scripts")));
 
