a9988b83c8cfde4fa661d59422567fc9888ef743
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 16:52:27 +0400

    Pushing to heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
