c44a969fa7f6e8db9f55a0716d2e57ed8b69934a
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 14:38:45 +0400

    Mongodb atlas working

--- browser.js
+++ browser.js
@@ -1,26 +1,26 @@
 window.addEventListener("load", async (event) => {
-	document.getElementById("form").addEventListener("submit", (event) => {
-		event.preventDefault();
-		let node = document.createElement("li");
-		node.innerText = document.getElementById("text-input").value;
-		document.getElementById("todo-list").appendChild(node);
+    document.getElementById("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        let node = document.createElement("li");
+        node.innerText = document.getElementById("text-input").value;
+        document.getElementById("todo-list").appendChild(node);
 
-		// send list item to server so it can put it into mongodb
-		const data = { data: node.innerText };
+        // send list item to server so it can put it into mongodb
+        const data = { data: node.innerText };
 
-		fetch("http://localhost:3000", {
-			method: "POST", // or 'PUT'
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log("Success:", data);
-			})
-			.catch((error) => {
-				console.error("Error:", error);
-			});
-	});
+        fetch("http://localhost:3000", {
+            method: "POST", // or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.text())
+            .then((text) => {
+                console.log(`${text}`);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    });
 });
--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const MongoClient = require("mongodb").MongoClient;
+const { response } = require("express");
 const url =
-	"mongodb+srv://dato:Irakli58@cluster0.m8xlq.mongodb.net/test?retryWrites=true&w=majority";
+    "mongodb+srv://dato:Irakli58@cluster0.m8xlq.mongodb.net/test?retryWrites=true&w=majority";
 
 app.get("/", (req, res) => {
-	// pull list items from server
-	res.send("Hello World!");
+    // pull list items from server
+    res.send("Hello World!");
 });
 
 app.post("/", (req, res) => {
-	//insert item into database
-	const list_item = req;
-	console.log(list_item);
-	const client = new MongoClient(url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
-	client.connect((err) => {
-		const collection = client.db("test").collection("list-items");
-		try {
-			collection.insertOne(list_item);
-		} catch (error) {
-			throw errror;
-		}
-	});
-	client.close();
-	res.send(`Got a new list item ${list_item.data}`);
+    //insert item into database
+    const list_item = req.body;
+    const client = new MongoClient(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    client.connect((err) => {
+        const collection = client.db("test").collection("list-items");
+        try {
+            collection.insertOne(list_item);
+            console.log(`Inserted ${list_item.data}`);
+        } catch (error) {
+            throw error;
+        }
+    });
+    client.close();
+    res.send(`Got a new list item ${list_item.data}`);
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
