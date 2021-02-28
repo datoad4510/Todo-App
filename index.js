258e74583812adb51344b73532b3e5d3816b037e
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 16:11:02 +0400

    Pushing to heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -56,5 +56,5 @@ app.post("/add_item", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at port ${port}`);
 });
