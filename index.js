5f4dffa42611b06ea881e88d3efd1d2cf7dee12d
user1 <54779055+datoad4510@users.noreply.github.com>
2021-02-28 16:58:25 +0400

    Pushing to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "Scripts")));
+// app.use(express.static(path.join(__dirname, "Scripts")));
 
 // app.use(function (req, res, next) {
 //     res.header(
